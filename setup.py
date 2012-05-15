71231d3510bfa13f463b008c64799dc6600a3064
Lukas Czerner <lczerner@redhat.com>
2012-05-15 10:28:07 +0200

    Update setup.py file
    Add some python modules whcih are required to run the script and also
    add data_files section to copy manual page to the system.
    
    Signed-off-by: Lukas Czerner <lczerner@redhat.com>

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
+import os
 import sys
 from distutils.core import setup
+
+VERSION='0.1-alpha'
+DOC_BUILD='doc/_build/'
 
 if sys.version < '2.7':
     print "Python version 2.7 or higher is required " + \
@@ -8,7 +12,7 @@ if sys.version < '2.7':
 
 setup(
     name='SystemStorageManager',
-    version='0.1dev',
+    version=VERSION,
     author='Lukas Czerner',
     author_email='lczerner@redhat.com',
     maintainer='Lukas Czerner',
@@ -18,6 +22,11 @@ setup(
     description='System Storage Manager - A single tool to manage your storage',
     license='GNU General Public License version 2 or any later version',
     long_description=open('README').read(),
-    requires=['argparse', 'itertools'],
+    requires=['argparse', 'itertools', 'tempfile', 'threading', 'subprocess',
+              'datetime', 're', 'os', 'sys', 'stat'],
     platforms=['Linux'],
+    data_files=[('/usr/share/man/man8', ['doc/_build/man/ssm.8']),
+                ('/usr/share/doc/ssm-{0}'.format(VERSION),
+                    ['README', 'TODO', 'CHANGES', 'COPYING'])]
 )
+
