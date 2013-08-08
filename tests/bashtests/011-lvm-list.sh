554675ff19d662db94eaadbc4997fa8692849cea
Jimmy Pan <jipan@redhat.com>
2013-08-08 15:30:15 +0800

    bashtests/011-lvm-list.sh: add 011-lvm-list.sh
    Test comes with tree scenarios, first is test without filesystem, second is test
    with filesystem, third is test with filesystem and mount points.
    Each scenario we check different ssm list commands, and list vol after resize
    Each scenario we create four pools.
    Fisrt pool has 1 device and 1 volume of full size
    Second pool has 1 device and half size
    Third pool has 2 devices and 1 volume of full size
    Fourth pool has 4 devices and three volume of different sizes
    
    Finally we check the listing when creating lvm volume of raid 0 with mounted and
    unmounted filesystem.
    
    This case is intended to replace 011-lvm-list.sh in the future
    
    [ Actually replace test 011 with this one immediately. Also change
    subject to cover the change. Some more adjustments of the test itself
    --Lukas ]
    
    Signed-off-by: Jimmy Pan <jipan@redhat.com>
    Signed-off-by: Lukas Czerner <lczerner@redhat.com>

--- tests/bashtests/011-lvm-list.sh
+++ tests/bashtests/011-lvm-list.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# (C)2012 Red Hat, Inc., Tom Marek <tmarek@redhat.com>
+# (C)2013 Red Hat, Inc., Jimmy Pan <jipan@redhat.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -21,9 +21,10 @@ export test_description='Check whether list command prints correct values for lv
 . lib/test
 
 DEV_COUNT=10
-DEV_SIZE=100
+DEV_SIZE=128
 TEST_MAX_SIZE=$(($DEV_COUNT*$DEV_SIZE))
 aux prepare_devs $DEV_COUNT $DEV_SIZE
+aux prepare_mnts 10
 TEST_DEVS=$(cat DEVICES)
 export LVOL_PREFIX="lvol"
 export SSM_DEFAULT_BACKEND='lvm'
@@ -33,8 +34,14 @@ export SSM_NONINTERACTIVE='1'
 snap1="snap1"
 snap2="snap2"
 
+lvol1=${LVOL_PREFIX}001
+lvol2=${LVOL_PREFIX}002
+lvol3=${LVOL_PREFIX}003
+
+pool0=$vg1
 pool1=$vg2
 pool2=$vg3
+pool3=$vg4
 
 TEST_FS=
 which mkfs.ext2 && TEST_FS+="ext2 "
@@ -45,28 +52,262 @@ which mkfs.xfs  && TEST_FS+="xfs"
 TEST_MNT=$TESTDIR/mnt
 [ ! -d $TEST_MNT ] && mkdir $TEST_MNT &> /dev/null
 
-##LVM
-# Check devices
-ssm add $TEST_DEVS
-ssm_output=$(ssm list dev)
-for device in ${TEST_DEVS}; do
-     check list_table "$ssm_output" $device 96.00MB 0.00KB 96.00MB $SSM_LVM_DEFAULT_POOL
+# Prepare pools and volumes
+
+vol1=volsf
+vol2=volss
+vol3=volmf
+vol4=volms1
+vol5=volms2
+vol6=volms3
+maxvolsz=$((DEV_SIZE-4))
+size1=$maxvolsz
+size2=$((DEV_SIZE/2))
+size3=$((maxvolsz*2))
+size4=$((DEV_SIZE/2))
+size5=$((DEV_SIZE*2))
+size6=$((DEV_SIZE/4))
+size4s=$((size4-20))
+size2r=$((size2-4))
+size5r=$((size5+16))
+
+
+# Test without a filesystem
+ssm -f create -n $vol1 $dev1
+ssm create -n $vol2 -p $pool1 -s ${size2}M $dev2
+ssm create -n $vol3 -p $pool2 $dev3 $dev4
+ssm add -p $pool3 $dev{5,6,7,8}
+ssm create -p $pool3 -s ${size4}m -n $vol4
+ssm create -p $pool3 -s ${size5}m -n $vol5
+ssm create -p $pool3 -s ${size6}m -n $vol6
+
+# We shouldn't see ssm list fs here
+test `ssm list fs | wc -l` -le 1
+# Check vol, dev, pool, resized vol, and snapshot
+output=`ssm list vol`
+check list_table "$output" $pool0/$vol1 $pool0 $((size1)).00MB linear
+check list_table "$output" $pool1/$vol2 $pool1 $size2.00MB linear
+check list_table "$output" $pool2/$vol3 $pool2 $((size3)).00MB linear
+check list_table "$output" $pool3/$vol4 $pool3 $((size4)).00MB linear
+check list_table "$output" $pool3/$vol5 $pool3 $((size5)).00MB linear
+check list_table "$output" $pool3/$vol6 $pool3 $((size6)).00MB linear
+output=`ssm list dev`
+check list_table "$output" $dev1 0.00KB $((size1)).00MB 124.00MB $pool0
+check list_table "$output" $dev2 $((maxvolsz-size2)).00MB $((size2)).00MB 124.00MB $pool1
+check list_table "$output" $dev3 0.00KB $((maxvolsz)).00MB 124.00MB $pool2
+check list_table "$output" $dev4 0.00KB $((maxvolsz)).00MB 124.00MB $pool2
+check list_table "$output" $dev5 none none 124.00MB $pool3
+check list_table "$output" $dev6 none none 124.00MB $pool3
+check list_table "$output" $dev7 none none 124.00MB $pool3
+check list_table "$output" $dev8 none none 124.00MB $pool3
+output=`ssm list pool`
+check list_table "$output" $pool0 lvm 1 0.00KB $((size1)).00MB $((maxvolsz)).00MB
+check list_table "$output" $pool1 lvm 1 $((maxvolsz-size2)).00MB $((size2)).00MB $((maxvolsz)).00MB
+check list_table "$output" $pool2 lvm 2 0.00KB $((size3)).00MB $((maxvolsz*2)).00MB
+check list_table "$output" $pool3 lvm 4 $((maxvolsz*4-size4-size5-size6)).00MB $((size4+size5+size6)).00MB $((maxvolsz*4)).00MB
+
+# Check ssm vol after resize
+ssm -f resize $pool1/$vol2 -s ${size2r}M
+ssm resize -s ${size5r}m $pool3/$vol5
+output=`ssm list vol`
+check list_table "$output" $pool1/$vol2 $pool1 $size2r.00MB linear
+check list_table "$output" $pool3/$vol5 $pool3 $((size5r)).00MB linear
+
+ssm snapshot $pool3/$vol4 -n snap1
+ssm snapshot $pool3/$vol4 -s ${size4s}m -n snap2
+output=`ssm list snap`
+check list_table "$output" $pool3/snap1 $vol4 $pool3 none none linear
+check list_table "$output" $pool3/snap2 $vol4 $pool3 $size4s.00MB none linear
+
+
+ssm -f remove -a
+
+
+# Test with filesystem
+for fs in $TEST_FS ; do
+	ssm -f create -n $vol1 $dev1 --fs $fs
+	ssm create -n $vol2 -p $pool1 -s ${size2}M $dev2 --fs $fs
+	ssm create -n $vol3 -p $pool2 $dev3 $dev4 --fs $fs
+	ssm add -p $pool3 $dev{5,6,7,8}
+	ssm create -p $pool3 -s ${size4}m -n $vol4 --fs $fs
+	ssm create -p $pool3 -s ${size5}m -n $vol5 --fs $fs
+	ssm create -p $pool3 -s ${size6}m -n $vol6 --fs $fs
+
+	# Check fs, vol, dev, pool, resized vol, and snapshot
+	output=`ssm list fs`
+	# xfs has some strange used size, so we don't check it
+	if [ $fs == xfs ] ; then
+		check list_table "$output" $pool0/$vol1 $pool0 $size1.00MB $fs none none linear
+		check list_table "$output" $pool1/$vol2 $pool1 $size2.00MB $fs none none linear
+		check list_table "$output" $pool2/$vol3 $pool2 $size3.00MB $fs none none linear
+		check list_table "$output" $pool3/$vol4 $pool3 $size4.00MB $fs none none linear
+		check list_table "$output" $pool3/$vol5 $pool3 $size5.00MB $fs none none linear
+		check list_table "$output" $pool3/$vol6 $pool3 $size6.00MB $fs none none linear
+	else
+		check list_table "$output" $pool0/$vol1 $pool0 $size1.00MB $fs $size1.00MB none linear
+		check list_table "$output" $pool1/$vol2 $pool1 $size2.00MB $fs $size2.00MB none linear
+		check list_table "$output" $pool2/$vol3 $pool2 $size3.00MB $fs $size3.00MB none linear
+		check list_table "$output" $pool3/$vol4 $pool3 $size4.00MB $fs $size4.00MB none linear
+		check list_table "$output" $pool3/$vol5 $pool3 $size5.00MB $fs $size5.00MB none linear
+		check list_table "$output" $pool3/$vol6 $pool3 $size6.00MB $fs $size6.00MB none linear
+	fi
+
+	output=`ssm list vol`
+	# xfs has some strange used size, so we don't check it
+	if [ $fs == xfs ] ; then
+		check list_table "$output" $pool0/$vol1 $pool0 $size1.00MB $fs none none linear
+		check list_table "$output" $pool1/$vol2 $pool1 $size2.00MB $fs none none linear
+		check list_table "$output" $pool2/$vol3 $pool2 $size3.00MB $fs none none linear
+		check list_table "$output" $pool3/$vol4 $pool3 $size4.00MB $fs none none linear
+		check list_table "$output" $pool3/$vol5 $pool3 $size5.00MB $fs none none linear
+		check list_table "$output" $pool3/$vol6 $pool3 $size6.00MB $fs none none linear
+	else
+		check list_table "$output" $pool0/$vol1 $pool0 $size1.00MB $fs $size1.00MB none linear
+		check list_table "$output" $pool1/$vol2 $pool1 $size2.00MB $fs $size2.00MB none linear
+		check list_table "$output" $pool2/$vol3 $pool2 $size3.00MB $fs $size3.00MB none linear
+		check list_table "$output" $pool3/$vol4 $pool3 $size4.00MB $fs $size4.00MB none linear
+		check list_table "$output" $pool3/$vol5 $pool3 $size5.00MB $fs $size5.00MB none linear
+		check list_table "$output" $pool3/$vol6 $pool3 $size6.00MB $fs $size6.00MB none linear
+	fi
+
+	output=`ssm list dev`
+	check list_table "$output" $dev1 0.00KB $((size1)).00MB 124.00MB $pool0
+	check list_table "$output" $dev2 $((maxvolsz-size2)).00MB $((size2)).00MB 124.00MB $pool1
+	check list_table "$output" $dev3 0.00KB $((maxvolsz)).00MB 124.00MB $pool2
+	check list_table "$output" $dev4 0.00KB $((maxvolsz)).00MB 124.00MB $pool2
+	check list_table "$output" $dev5 none none 124.00MB $pool3
+	check list_table "$output" $dev6 none none 124.00MB $pool3
+	check list_table "$output" $dev7 none none 124.00MB $pool3
+	check list_table "$output" $dev8 none none 124.00MB $pool3
+	output=`ssm list pool`
+	check list_table "$output" $pool0 lvm 1 0.00KB $((size1)).00MB $((maxvolsz)).00MB
+	check list_table "$output" $pool1 lvm 1 $((maxvolsz-size2)).00MB $((size2)).00MB $((maxvolsz)).00MB
+	check list_table "$output" $pool2 lvm 2 0.00KB $((size3)).00MB $((maxvolsz*2)).00MB
+	check list_table "$output" $pool3 lvm 4 $((maxvolsz*4-size4-size5-size6)).00MB $((size4+size5+size6)).00MB $((maxvolsz*4)).00MB
+
+	# Check ssm vol after resize
+	# xfs size cannot reduce
+	if [ "$fs" != xfs ] ; then
+		ssm -f resize $pool1/$vol2 -s ${size2r}M
+	fi
+	ssm resize -s ${size5r}m $pool3/$vol5
+
+	output=`ssm list vol`
+	# xfs has some strange used size, so we don't check it
+	if [ "$fs" != xfs ] ; then
+		check list_table "$output" $pool1/$vol2 $pool1 $size2r.00MB $fs $size2r none linear
+		check list_table "$output" $pool3/$vol5 $pool3 $size5r.00MB $fs $size5r none linear
+	else
+		check list_table "$output" $pool3/$vol5 $pool3 $size5r.00MB $fs none none linear
+	fi
+
+	ssm snapshot $pool3/$vol4 -n snap1
+	ssm snapshot $pool3/$vol4 -s ${size4s}m -n snap2
+
+	output=`ssm list snap`
+	check list_table "$output" $pool3/snap1 $vol4 $pool3 none none linear
+	check list_table "$output" $pool3/snap2 $vol4 $pool3 $size4s.00MB none linear
+
+	ssm -f remove -a
+
 done
 
-# Check pools
-check list_table "$(ssm list pool)" $SSM_LVM_DEFAULT_POOL $SSM_DEFAULT_BACKEND $DEV_COUNT none none 960.00MB
-ssm -f remove --all
+# Test with a mountpoint
+for fs in $TEST_FS ; do
+	ssm -f create -n $vol1 --fs $fs $dev1 $mnt1
+	ssm create -n $vol2 -p $pool1 -s ${size2}M --fs $fs $dev2 $mnt2
+	ssm create -n $vol3 -p $pool2 --fs $fs $dev3 $dev4 $mnt3
+	ssm add -p $pool3 $dev{5,6,7,8}
+	ssm create -p $pool3 -s ${size4}m -n $vol4 --fs $fs $mnt4
+	ssm create -p $pool3 -s ${size5}m -n $vol5 --fs $fs $mnt5
+	ssm create -p $pool3 -s ${size6}m -n $vol6 --fs $fs $mnt6
 
-# create multiple pools
-ssm create --pool $pool1 $dev1 $dev2 $dev3 $dev4
-ssm create --pool $pool2 $dev5 $dev6 $dev7 $dev8
-ssm_output=$(ssm list pool)
-check list_table "$ssm_output" $pool1 lvm 4 none none 384.00MB
-check list_table "$ssm_output" $pool2 lvm 4 none none 384.00MB
-ssm -f remove --all
+	# Check fs, vol, dev, pool, resized vol, and snapshot
+	output=`ssm list fs`
+	# xfs has some strange used size, so we don't check it
+	if [ $fs == xfs ] ; then
+		check list_table "$output" $pool0/$vol1 $pool0 $size1.00MB $fs none none linear $mnt1
+		check list_table "$output" $pool1/$vol2 $pool1 $size2.00MB $fs none none linear $mnt2
+		check list_table "$output" $pool2/$vol3 $pool2 $size3.00MB $fs none none linear $mnt3
+		check list_table "$output" $pool3/$vol4 $pool3 $size4.00MB $fs none none linear $mnt4
+		check list_table "$output" $pool3/$vol5 $pool3 $size5.00MB $fs none none linear $mnt5
+		check list_table "$output" $pool3/$vol6 $pool3 $size6.00MB $fs none none linear $mnt6
+	else
+		check list_table "$output" $pool0/$vol1 $pool0 $size1.00MB $fs $size1.00MB none linear $mnt1
+		check list_table "$output" $pool1/$vol2 $pool1 $size2.00MB $fs $size2.00MB none linear $mnt2
+		check list_table "$output" $pool2/$vol3 $pool2 $size3.00MB $fs $size3.00MB none linear $mnt3
+		check list_table "$output" $pool3/$vol4 $pool3 $size4.00MB $fs $size4.00MB none linear $mnt4
+		check list_table "$output" $pool3/$vol5 $pool3 $size5.00MB $fs $size5.00MB none linear $mnt5
+		check list_table "$output" $pool3/$vol6 $pool3 $size6.00MB $fs $size6.00MB none linear $mnt6
+	fi
+
+	output=`ssm list vol`
+	# xfs has some strange used size, so we don't check it
+	if [ $fs == xfs ] ; then
+		check list_table "$output" $pool0/$vol1 $pool0 $size1.00MB $fs none none linear $mnt1
+		check list_table "$output" $pool1/$vol2 $pool1 $size2.00MB $fs none none linear $mnt2
+		check list_table "$output" $pool2/$vol3 $pool2 $size3.00MB $fs none none linear $mnt3
+		check list_table "$output" $pool3/$vol4 $pool3 $size4.00MB $fs none none linear $mnt4
+		check list_table "$output" $pool3/$vol5 $pool3 $size5.00MB $fs none none linear $mnt5
+		check list_table "$output" $pool3/$vol6 $pool3 $size6.00MB $fs none none linear $mnt6
+	else
+		check list_table "$output" $pool0/$vol1 $pool0 $size1.00MB $fs $size1.00MB none linear $mnt1
+		check list_table "$output" $pool1/$vol2 $pool1 $size2.00MB $fs $size2.00MB none linear $mnt2
+		check list_table "$output" $pool2/$vol3 $pool2 $size3.00MB $fs $size3.00MB none linear $mnt3
+		check list_table "$output" $pool3/$vol4 $pool3 $size4.00MB $fs $size4.00MB none linear $mnt4
+		check list_table "$output" $pool3/$vol5 $pool3 $size5.00MB $fs $size5.00MB none linear $mnt5
+		check list_table "$output" $pool3/$vol6 $pool3 $size6.00MB $fs $size6.00MB none linear $mnt6
+	fi
+
+	output=`ssm list dev`
+	check list_table "$output" $dev1 0.00KB $((size1)).00MB 124.00MB $pool0
+	check list_table "$output" $dev2 $((maxvolsz-size2)).00MB $((size2)).00MB 124.00MB $pool1
+	check list_table "$output" $dev3 0.00KB $((maxvolsz)).00MB 124.00MB $pool2
+	check list_table "$output" $dev4 0.00KB $((maxvolsz)).00MB 124.00MB $pool2
+	check list_table "$output" $dev5 none none 124.00MB $pool3
+	check list_table "$output" $dev6 none none 124.00MB $pool3
+	check list_table "$output" $dev7 none none 124.00MB $pool3
+	check list_table "$output" $dev8 none none 124.00MB $pool3
+	output=`ssm list pool`
+	check list_table "$output" $pool0 lvm 1 0.00KB $((size1)).00MB $((maxvolsz)).00MB
+	check list_table "$output" $pool1 lvm 1 $((maxvolsz-size2)).00MB $((size2)).00MB $((maxvolsz)).00MB
+	check list_table "$output" $pool2 lvm 2 0.00KB $((size3)).00MB $((maxvolsz*2)).00MB
+	check list_table "$output" $pool3 lvm 4 $((maxvolsz*4-size4-size5-size6)).00MB $((size4+size5+size6)).00MB $((maxvolsz*4)).00MB
+
+	# Check ssm vol after resize
+	# xfs size cannot reduce
+	if [ "$fs" != xfs ] ; then
+		umount $mnt2
+		ssm -f resize $pool1/$vol2 -s ${size2r}M
+	fi
+	ssm resize -s ${size5r}m $pool3/$vol5
+
+	output=`ssm list vol`
+	# xfs has some strange used size, so we don't check it
+	if [ "$fs" != xfs ] ; then
+		check list_table "$output" $pool1/$vol2 $pool1 $size2r.00MB $fs $size2r none linear
+		check list_table "$output" $pool3/$vol5 $pool3 $size5r.00MB $fs $size5r none linear
+	else
+		check list_table "$output" $pool3/$vol5 $pool3 $size5r.00MB $fs none none linear
+	fi
+
+	ssm snapshot $pool3/$vol4 -n snap1
+	ssm snapshot $pool3/$vol4 -s ${size4s}m -n snap2
+
+	output=`ssm list snap`
+	check list_table "$output" $pool3/snap1 $vol4 $pool3 none none linear
+	check list_table "$output" $pool3/snap2 $vol4 $pool3 $size4s.00MB none linear
+
+	for i in {1..6} ; do
+		mntdir=`eval echo '$mnt'$i`
+		umount $mntdir || true
+	done
+
+	ssm -f remove -a
+
+done
 
 ssm add $TEST_DEVS
-
 # Check LVM volume listings with various fs
 for fs in $TEST_FS; do
     name="${fs}vol"
@@ -85,30 +326,5 @@ for fs in $TEST_FS; do
 done
 ssm -f remove $SSM_LVM_DEFAULT_POOL
 
-# Check lvm snapshot
-lvol1=${LVOL_PREFIX}001
-# Create volume with all devices at once
-size=$(($DEV_SIZE*6))
-snap_size1=$(($DEV_SIZE))
-snap_size2=$(($size/5))
-ssm create --size ${size}M $TEST_DEVS
-ssm snapshot --name $snap1 --size ${snap_size1}M $SSM_LVM_DEFAULT_POOL/$lvol1
-ssm snapshot --name $snap2 --size ${snap_size2}M $SSM_LVM_DEFAULT_POOL/$lvol1
-ssm_output=$(ssm list snap)
-check list_table "$ssm_output" $snap1 $lvol1 $SSM_LVM_DEFAULT_POOL ${snap_size1}.00MB 0.00KB linear
-check list_table "$ssm_output" $snap2 $lvol1 $SSM_LVM_DEFAULT_POOL ${snap_size2}.00MB 0.00KB linear
-ssm -f remove --all
-
-# Snapshot of the volumes in defferent pools
-ssm create --pool $pool1 $dev1 $dev2
-ssm add $dev3 $dev4 --pool $pool1
-ssm create --pool $pool2 $dev5 $dev6
-ssm add $dev7 $dev8 --pool $pool2
-ssm snapshot --name $snap1 $pool1/$lvol1
-ssm snapshot --name $snap1 $pool2/$lvol1
-ssm_output=$(ssm list snap)
-check list_table "$ssm_output" "$pool1/$snap1" $lvol1 $pool1 40.00MB 0.00KB linear
-check list_table "$ssm_output" "$pool2/$snap1" $lvol1 $pool2 40.00MB 0.00KB linear
-ssm -f remove --all
-
-# all_done!!!
+# Some situation should fail
+not ssm list wrong_type
